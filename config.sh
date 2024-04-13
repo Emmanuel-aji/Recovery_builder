593b9b2162ada4595e9d4e202075db5edcc4ddbd
Emmanuel-aji <111465066+Emmanuel-aji@users.noreply.github.com>
2024-04-13 21:28:02 +0100

    Update config.sh

--- config.sh
+++ config.sh
@@ -3,13 +3,14 @@
 # about the custom recovery
 export NAME="twrp" # supported: pbrp, shrp, twrp
 export MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git" # the link of manifest
-export BRANCH="twrp-9.0" # the branch of manifest
+export BRANCH="twrp-8.1" # the branch of manifest
 
 # about your device
-export DEVICE="m10lte" # codename used in device tree
-export DT_LINK="https://github.com/youssefnone/android_recovery_samsung_m10lte" # device tree link
-export DT_BRANCH="twrp" # device tree branch
-export VENDOR="samsung" # device manufacturer or vendor
+export DEVICE="Infinix-X650" # codename used in device tree
+export DT_LINK="https://github.com/twrpdtgen/android_device_infinix_Infinix-X650/tree/full_x650_h8029-user-8.1.0-O11019-1626426640-release-keys" # device tree link
+export DT_BRANCH="full_x650_h8029-user-8.1.0-O11019-1626426640-release-keys"
+#default # device tree branch
+export VENDOR="infinix" # device manufacturer or vendor
 export TARGET="recoveryimage" # build method: recoveryimage, bootimage, vendorboot
 export TYPE="omni" # recovery type: omni, twrp, something else
 
