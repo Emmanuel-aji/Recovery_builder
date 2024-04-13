939abd28a50e65c25420d2b90bde96f25968a6ab
Emmanuel-aji <111465066+Emmanuel-aji@users.noreply.github.com>
2024-04-13 21:57:55 +0100

    Update config.sh

--- config.sh
+++ config.sh
@@ -7,7 +7,7 @@ export BRANCH="twrp-8.1" # the branch of manifest
 
 # about your device
 export DEVICE="Infinix-X650" # codename used in device tree
-export DT_LINK="https://github.com/twrpdtgen/android_device_infinix_Infinix-X650/tree/full_x650_h8029-user-8.1.0-O11019-1626426640-release-keys" # device tree link
+export DT_LINK="https://github.com/twrpdtgen/android_device_infinix_Infinix-X650" # device tree link
 export DT_BRANCH="full_x650_h8029-user-8.1.0-O11019-1626426640-release-keys"
 #default # device tree branch
 export VENDOR="infinix" # device manufacturer or vendor
